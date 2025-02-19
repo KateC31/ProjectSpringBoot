94f1084677f9014169e0ccb583a91befd210c1ca
Paola Caballero <paolisco2525@gmail.com>
2025-02-19 13:10:30 -0500

    Creación proyecto spring boot

---
+++ DataBaseInti/src/main/java/com/example/databaseinti/DataBaseIntiApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseinti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseIntiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseIntiApplication.class, args);
+    }
+
+}
---
+++ DataBaseInti/src/test/java/com/example/databaseinti/DataBaseIntiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databaseinti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseIntiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
